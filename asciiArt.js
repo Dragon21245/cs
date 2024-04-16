828c6511492ef38b5ad1f2af4377d75c42b14c34
Andrew Phan <phanand@oregonstate.edu>
2024-04-16 00:08:30 -0700

    Add Figlet ASCII art and updated package.json

--- asciiArt.js
+++ asciiArt.js
@@ -1,6 +1,20 @@
-const userInput = process.argv[2];
+/*const userInput = process.argv[2];
 if (!userInput) {
     console.log("Please provide a string as the first command line argument.");
 } else {
     console.log("String to turn into ASCII art:", userInput);
+} */
+
+const figlet = require('figlet');
+const userInput = process.argv[2];
+if (!userInput) {
+    console.log("Please provide a string as the command line argument.");
+} else {
+    figlet.text(userInput, function(err, data) {
+        if (err) {
+            console.log('Error:', err);
+        } else {
+            console.log(data);
+        }
+    });
 }
